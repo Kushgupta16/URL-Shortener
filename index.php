cfdec9e20e091495fcef108bd0106663bd68960a
Kush Gupta <97805045+Kushgupta16@users.noreply.github.com>
2023-10-22 04:35:51 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>URL Shortner</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="http://unicons.iconscout.com/release/v3.0.6/css/line.css">
+</head>
+<body>
+    <div class="wrapper">
+        <form action="#" autocomplete="off">
+            <input type="text" placeholder="Enter or paste a long url" required>
+            <i class="url-icon uil uil-link"></i>
+            <button>Shorten</button> 
+        </form>
+        <div class="count">
+            <span>Total Links: <span>10</span> & Total Clicks: <span>140</span></span>
+            <a href='#'>Clear All</a>
+        </div>
+        <div class ="urls-area">
+            <div class ="title">
+                <li>Shorten URL</li>
+                <li>Original URL</li>
+                <li>Clicks</li>
+                <li>Action</li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.comadasddasdsadsadasdsadasdsadasdasdsadasd</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class ="data">
+                <li><a href='#'>example.com/xyz234</a></li>
+                <li>www.google.com</li>
+                <li>16</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+        </div>
+    </div>
+
+    <div class="blur-effect"></div>
+    <div class="popup-box">
+        <div class="info-box">Your link is ready</div>
+        <form action="#">
+            <label>Edit The URL</label>
+            <input type="text" spellcheck="false" value="example.com/xyz234">
+            <i class="copy-icon uil uil-copy-alt"></i>
+            <button>Save</button>
+        </form>
+    </div>
+
+<script src="script.js"></script>
+    
+</body>
+</html>
